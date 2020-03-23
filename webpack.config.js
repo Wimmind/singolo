00c2d9b2d698f05cad9d056fb3831ce8d0359565
Wimmind <terehov.anton@bk.ru>
2020-03-23 13:23:40 +0400

    fix: fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,69 +4,62 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = (env, options) => {
-    const isProduction = options.mode === 'production';
-
-    const config = {
-        mode: isProduction ? 'production' : 'development',
-        devtool: isProduction ? 'none' : 'source-map',
-        watch: !isProduction,
-        entry: ['./src/index.js','./src/styles.scss'],
-        output: {
-            path: path.join(__dirname, '/dist'),
-            filename: 'script.js',
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: {
-                      loader: 'babel-loader',
-                      options: {
-                        presets: ['@babel/preset-env']
-                      }
+module.exports = {
+    watch: true,
+    entry: ['./src/index.js','./src/styles.scss'],
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'script.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: false,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                    presets: ['@babel/preset-env']
                     }
-                },
-                {
-                    test: /\.(sa|sc|c)ss$/,
-                    use: [
-                        MiniCssExtractPlugin.loader,'css-loader','sass-loader'
-                    ]
-                },
-                {
-                    test: /\.(woff|woff2|eot|ttf|otf)$/,
-                    use: [
-                      {
-                        loader: 'file-loader',
-                        options: {outputPath: './src/assets/fonts' }
-                      }
-                    ]
-                },
-                {
-                    test: /\.(png|svg|jpe?g|gif)$/,
-                    use: [
-                      {
-                        loader: 'file-loader',
-                      }
-                    ]
-                },
-                {
-                    test: /\.html$/,
-                    loader: 'html-loader',
                 }
-            ]
-        },
-        plugins: [
-            new CleanWebpackPlugin(),
-            new HtmlWebpackPlugin({
-                template: 'index.html'
-            }),
-            new MiniCssExtractPlugin({
-                filename: 'style.css'
-            })
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,'css-loader','sass-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                loader: 'file-loader',
+                options: { outputPath: 'assets/fonts',name: "[name].[ext]"},
+            },
+            {
+                test: /\.(png|svg|jpe?g|gif)$/i,
+                loader: 'file-loader',
+                options: { outputPath: 'assets/image',name: "[name].[ext]" },
+            }
         ]
-    }
-
-    return config;
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        })
+    ]
 }
